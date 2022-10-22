cb72bfbdf167616e05aded9f8d58238051ac18d1
theata-prog <yudengdaotian@gmail.com>
2022-10-22 18:24:42 +0900

    test

--- js/app.js
+++ js/app.js
@@ -99,14 +99,43 @@ SQR.modal = (() => {
     const modal = document.querySelector('#js-modal')
     const modalClose = document.querySelector('#js-modal-close')
 
-    /**
-     * 取得した文字列を入れ込んでモーダルを開く
-     */
-    const open = (url) => {
-        result.value = url
-        link.setAttribute('href', url)
-        modal.classList.add('is-show')
-    }
+    // /**
+    //  * 取得した文字列を入れ込んでモーダルを開く
+    //  */
+    // const open = (url) => {
+    //     result.value = url
+    //     link.setAttribute('href', url)
+    //     modal.classList.add('is-show')
+    // }
+    
+    const move = (val) => {
+        result.value = val
+        let test1 = "content1"
+        let test2 = "content2"
+        let test3 = "content3"
+        let targetURL = "https://chico-shikaku.com"
+        let json_asocc =
+         
+            {
+                'test1'     : test1,
+                'test2'     : test2,
+                'test3'     : test3
+            };
+         
+        let json_text = JSON.stringify(json_asocc);
+        let testRequest = new XMLHttpRequest();
+        testRequest.onload = function (){ 
+            let data = this.response;
+            console.log(data)
+        }
+        testRequest.onerror = function (){
+            console.log('Could not post');
+        }
+        testRequest.open('POST', targetURL);
+        testRequest.setRequestHeader('content-type', 'application/json');
+        testRequest.send(json_text);
+    } 
+
 
     /**
      * モーダルを閉じてQR読み込みを再開
@@ -126,7 +155,7 @@ SQR.modal = (() => {
     modalClose.addEventListener('click', () => close())
 
     return {
-        open,
+        move,
     }
 })()
 
