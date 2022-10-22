c8d054080480656071b22ade399ed3ded6c6b744
theata-prog <yudengdaotian@gmail.com>
2022-10-23 00:34:11 +0900

    fix

--- js/app.js
+++ js/app.js
@@ -102,26 +102,9 @@ SQR.modal = (() => {
     /**
      * 取得した文字列を入れ込んでモーダルを開く
      */
-    const open = (json) => {
-        result.value = json
-        // link.setAttribute('href', json)
-        const data = result.value;
-
-        fetch('https://example.com/profile', {
-            method: 'POST', // or 'PUT'
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-
+    const open = (url) => {
+        result.value = url
+        link.setAttribute('href', url)
         modal.classList.add('is-show')
     }
 
